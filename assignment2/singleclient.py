bf59a1fc738c9fcd082e4567364956c582b313cf
Rajat Golechha <98651238+rajatgolechha3319@users.noreply.github.com>
2023-08-22 21:08:54 +0530

    Updated singleclient.py
    check this everyone

--- assignment2/singleclient.py
+++ assignment2/singleclient.py
@@ -1,103 +1,89 @@
-import socket
-import time
+import socket,time
 
-
-
-def extract_num_lines(submission_response):
+def inc_num_lines(submission_response):
     parts = submission_response.split(" - ")
     if len(parts) >= 3:
         num_lines_part = parts[2]
-        num_lines = int(num_lines_part.split(", ")[5])
+        num_lines = int(num_lines_part.split(", ")[1])
         return num_lines
     return None
+  
+def recv_input(sock):
+  """
+  Receive input in packets that are coming in the format `1\nsome message here\n`.
 
+  Args:
+    sock: The socket object.
 
-def maxlines():
-    server_address = ("vayu.iitd.ac.in", 9801)
-    sendline_command = b"SENDLINE\n"
-    submit_command = b"SUBMIT\naseth@col334-672\n1\n1\n"
+  Returns:
+    The entire input as a string.
+  """
 
-    try:
+  input_buffer = b''
+  while True:
+    packet = sock.recv(1024)
+    input_buffer += packet
+    if packet.endswith(b'\n'):
+      break
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(5)
-            s.connect(server_address)
-            flag = 0
-            
-            while(flag==0):
+  # The last packet does not end with a newline, so we need to add it manually.
 
-                s.sendall(sendline_command)
-                initial_response = s.recv(4096).decode("utf-8")
+  if not input_buffer.endswith(b'\n'):
+    input_buffer += b'\n'
 
-                if initial_response.strip() != "-1":
-                    flag = 1 
-                    num_lines = int(initial_response.split("\n")[0])  
-                    submission_payload = submit_command + str(num_lines).encode() + b"\"Line 1\"\n"
-    
-                    # Send the submission payload
+  return input_buffer.decode('utf-8')
 
-                    s.sendall(submission_payload)
-                    submission_success = s.recv(4096).decode("utf-8")
-                    num_lines = extract_num_lines(submission_success)
-                    # print(num_lines)
-                    return num_lines
-
-    except (socket.timeout, ConnectionError):
-        pass
-    except Exception as e:
-        pass
-
-
-
-def receive_all(sock, buffer_size=4096):
-    received_data = b""
-    while True:
-        packet = sock.recv(buffer_size)
-        if not packet:
-            break
-        received_data += packet
-        if b"\n" in packet:
-            break
-    return received_data
 
 def main():
     server_address = ("vayu.iitd.ac.in", 9801)
     sendline_command = b"SENDLINE\n"
-    line_num = maxlines()
+    line_num = 1000
     data_dict = {}
     try:
+        start = time.time()
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(5)
             s.connect(server_address)
-
-            while(len(data_dict) < line_num):
+            while len(data_dict) < line_num:
                 print(len(data_dict))
                 s.sendall(sendline_command)
-                received_data = receive_all(s).decode("utf-8")
-                if received_data.strip() != "-1":
-                    
-                    try:
-                        line_number = int(received_data.strip().split("\n")[0])
-                    except ValueError:
-                        continue  # Continue to the next iteration of the loop
-
-                    line_content = received_data.strip().split("\n")[1]
-                    if line_number not in data_dict:
-                        data_dict[line_number] = line_content
-                time.sleep(1e-8)
-        
-
+                received_data = recv_input(s)
+                try:
+                    lines = received_data.split("\n")
+                    line_number = int(lines[0])
+                    line_content = lines[1]
+                except (ValueError, IndexError):
+                    continue
+                if line_number == -1:
+                    time.sleep(1e-6)
+                    continue
+                if line_number not in data_dict:
+                    data_dict[line_number] = line_content
+            file_name = "output.txt"
+            with open(file_name, "w") as file:
+                for key, value in data_dict.items():
+                    file.write(f"Key: {key}, Value: {value}\n")
+            print(f"Data written to {file_name}")
+            print("Submitting...")
+            submit_command = b"SUBMIT\naseth@col334-672\n" + str(line_num).encode() + b"\n"
+            s.sendall(submit_command)
+            for key in data_dict.keys():
+                s.sendall(str(key).encode() + b"\n" + data_dict[key].encode() + b"\n")
+            submission_success = recv_input(s)
+            print(submission_success)
+            inc = inc_num_lines(submission_success)
+            if(inc > 0):
+              s.sendall(b"SEND INCORRECT LINES\n")
+              for i in range(inc):
+                data = recv_input(s)
+                print(data)
+        finish = time.time()
+        print(f"Time taken: {finish - start}")
+            
     except (socket.timeout, ConnectionError):
         print("Connection error")
-        pass
     except Exception as e:
         print("An error occurred:", e)
-        pass
-    
-    
-    
 
 if __name__ == "__main__":
     main()
-
-
