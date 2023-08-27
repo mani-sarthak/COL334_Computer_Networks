b63132bd934dde6f97fb81fd1407de23d88deb4e
Rajat Golechha <98651238+rajatgolechha3319@users.noreply.github.com>
2023-08-27 14:44:06 +0530

    Update 27thserver.py

--- assignment2/27thserver.py
+++ assignment2/27thserver.py
@@ -1,35 +1,43 @@
 import socket
 import threading
-server_list=[]
+
+server_list = []
+broadcast_list = []
+broadcast_lock = threading.Lock()
+
 def handle_client(conn, address):
     while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        print("Connection from: " + str(address))
         data = conn.recv(2048).decode()
         if not data:
-            # if data is not received break
             break
-        print("from connected user: " + str(data))
-        for k in server_list:
-            # k.send(response.encode())
-            k.send(data.encode())
-        response = input(' -> ')
-        for k in server_list:
-            k.send(response.encode())
-            # k.send(data.encode())
-        # conn.send(response.encode())  # send data to the client
+        print("Received from connected user {}: {}".format(address, data))
+        with broadcast_lock:
+            for client_conn in server_list:
+                client_conn.send(data.encode())
     conn.close()
 
+def broadcast_thread():
+    while True:
+        message = input("Broadcast message: ")
+        with broadcast_lock:
+            for client_conn in server_list:
+                client_conn.send(message.encode())
+
 def server_program():
-    host = '192.168.197.164'
-    port = 7201
-    thread_list=[]
+    host = '192.168.197.228'
+    port = 7215
+    thread_list = []
+
     server_socket = socket.socket()
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # wrong line
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((host, port))
     server_socket.listen(4)
 
     print("Server listening on {}:{}".format(host, port))
+
+    receive_thread = threading.Thread(target=broadcast_thread)
+    receive_thread.start()
+    thread_list.append(receive_thread)
 
     while True:
         conn, address = server_socket.accept()
@@ -37,10 +45,9 @@ def server_program():
         client_thread = threading.Thread(target=handle_client, args=(conn, address))
         client_thread.start()
         thread_list.append(client_thread)
+
     for k in thread_list:
         k.join()
-    
-    
 
 if __name__ == '__main__':
     server_program()
