3de50b5559f8c143d70e91d489432254a1d9faac
harshit gupta <97735279+harshitgupta3006@users.noreply.github.com>
2023-08-27 13:38:23 +0530

    Update server.cpp

--- assignment2/server.cpp
+++ assignment2/server.cpp
@@ -1,67 +1,67 @@
 #include <iostream>
-#include <cstring>
 #include <cstdlib>
+#include <cstring>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 #include <unistd.h>
 
 int main() {
-    // Create a socket
+    // Create socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
-        std::cerr << "Error creating socket" << std::endl;
-        return 1;
+        std::cerr << "Error creating socket." << std::endl;
+        return -1;
     }
 
-    // Bind the socket
+    // Bind socket
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(8001); // Port number
+    serverAddress.sin_port = htons(9001);  // Port number
 
     if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
-        std::cerr << "Error binding socket" << std::endl;
-        return 1;
+        std::cerr << "Error binding socket." << std::endl;
+        return -1;
     }
 
-    // Listen for incoming connections
+    // Listen for connections
     if (listen(serverSocket, 5) == -1) {
-        std::cerr << "Error listening on socket" << std::endl;
-        return 1;
+        std::cerr << "Error listening on socket." << std::endl;
+        return -1;
     }
 
-    std::cout << "Server is listening for incoming connections..." << std::endl;
+    std::cout << "Server waiting for incoming connections..." << std::endl;
 
-    // Accept and handle client connections
+    // Accept connections
+    int clientSocket;
+    sockaddr_in clientAddress;
+    socklen_t clientAddrSize = sizeof(clientAddress);
+
+    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddrSize);
+    if (clientSocket == -1) {
+        std::cerr << "Error accepting connection." << std::endl;
+        return -1;
+    }
+
+    std::cout << "Client connected." << std::endl;
+
+    // Send and receive messages
+    char message[1024];
     while (true) {
-        sockaddr_in clientAddress;
-        socklen_t clientAddrSize = sizeof(clientAddress);
-        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddrSize);
-        if (clientSocket == -1) {
-            std::cerr << "Error accepting client connection" << std::endl;
-            continue;
-        }
+        memset(message, 0, sizeof(message));
 
-        std::cout << "Client connected" << std::endl;
+        // Receive message from client
+        recv(clientSocket, message, sizeof(message), 0);
+        std::cout << "Client: " << message << std::endl;
 
-        // Handle client messages
-        char buffer[1024];
-        while (true) {
-            int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-            if (bytesRead <= 0) {
-                std::cout << "Client disconnected" << std::endl;
-                close(clientSocket);
-                break;
-            }
-
-            buffer[bytesRead] = '\0';
-            std::cout << "Client: " << buffer << std::endl;
-
-            // Send a response back to the client
-            send(clientSocket, buffer, bytesRead, 0);
-        }
+        std::cout << "-> ";
+        // Send message to client
+        std::cin.getline(message, sizeof(message));
+        send(clientSocket, message, strlen(message), 0);
     }
 
+    // Close sockets
+    close(clientSocket);
     close(serverSocket);
+ 
     return 0;
 }
