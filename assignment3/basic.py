7fc6772b198985c7da413bd843571e8464f6265b
mani-sarthak <manisarthakiitd@gmail.com>
2023-09-30 00:10:05 +0530

    code to send data and accept

--- assignment3/basic.py
+++ assignment3/basic.py
@@ -1,18 +1,61 @@
 import socket
+import re
+import time
 
-server_address = ('vayu.iitd.ac.in', 9801)  # Replace with your server's address and port
+server_address = ('127.0.0.1', 9802)  # Replace with your server's address and port
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+
+
+def getSize(data):
+    match = re.search(r'Size:\s+(\d+)', data)
+    return int(match.group(1))
+    
 try:
     # Send data to the server
-    message = 'SENDLINE\n'  # Your message here
+    message = 'SendSize\n\n'  # Your message here
     sock.sendto(message.encode(), server_address)
 
     # Receive a response from the server (optional)
-    data, server = sock.recvfrom(4096)
-    print(f'Received: {data.decode()}')
-    print(server)
+    data, server = sock.recvfrom(2096)
+    data = data.decode()
+    # print(data)
+    size = getSize(data)
+    print(size)
+    # print(server)
 
 finally:
     # Clean up the socket
-    sock.close()
+    print('Done')
+
+
+
+offset = 0
+maxSize = 1400 ## 1448 could be max just to be on safer side
+arr = [(x, min(x+maxSize, size)-x) for x in range(0, size, maxSize)]
+# print(arr)
+
+requests = len(arr)
+d = dict()
+i = 0
+while (len(d) != requests):
+    i = i % requests
+    offset = arr[i][0]
+    size = arr[i][1]
+    message = f'Offset: {offset}\nNumBytes: {size}\n\n'
+    if size > 0:
+        try:
+            sock.sendto(message.encode(), server_address)
+            data, server = sock.recvfrom(2096)
+            data = data.decode()
+            print('\n\n\n\n', data, '\n\n\n\n')
+            d[i] = data
+            arr[i][1] = 0
+        except:
+            print('cant fetch data', offset, size)
+        
+    time.sleep(5)
+    print(len(d))
+    i += 1
+    
+print(d)
